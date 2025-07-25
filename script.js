8e191010bd7d95c69a5f3e3a916f5a7398abefa1
Arnab Das <darnab628@gmail.com>
2025-07-25 13:28:22 +0530

    Code done for a single round

--- script.js
+++ script.js
@@ -15,7 +15,48 @@ const getComputerChoice =()=>{
 
 const getHumanChoice = () =>{
     let choice = prompt("Your turn")
-    return choice;
+    return choice.toLowerCase();
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound=(humanChoice,computerChoice)=>{
+    if(humanChoice === "rock"){
+        switch (computerChoice) {
+            case "paper":
+                return "You loose paper beats rock"
+            case "scissors":
+                return "You win rock beats scissors"
+            default:
+                return "Oops Draw !"
+        }
+    }
+    else if(humanChoice === "paper"){
+        switch (computerChoice) {
+            case "rock":
+                return "You win paper beats rock"
+            case "scissors":
+                return "You loose scissors beats paper"
+            default:
+                return "Oops Draw !"
+        }
+    }
+    else{
+        switch (computerChoice) {
+            case "rock":
+                return "You loose rock beats scissors"
+            case "paper":
+                return "You win scissors beats paper"
+            default:
+                return "Oops Draw !"
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(`Human : ${humanSelection}`)
+console.log(`Computer : ${computerSelection}`)
+console.log(playRound(humanSelection,computerSelection))

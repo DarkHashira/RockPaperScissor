543d1c8fde9854cfdabf5c9ddff25c024519ab6e
Arnab Das <darnab628@gmail.com>
2025-07-25 13:02:27 +0530

    Fucntion for random value created

---
+++ script.js
@@ -0,0 +1,16 @@
+const getComputerChoice =()=>{
+    let choice = Math.floor(Math.random()*3)
+
+    switch (choice) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+        default:
+            return "error"
+    }
+}
+
+console.log(getComputerChoice())

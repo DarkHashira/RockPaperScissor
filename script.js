199c4b60f6d2656183cb8ef10458f94df929dc35
Arnab Das <darnab628@gmail.com>
2025-07-25 13:56:04 +0530

    i added choice for how many rounds wants to play loop will run that many times

--- script.js
+++ script.js
@@ -1,62 +1,88 @@
-const getComputerChoice =()=>{
-    let choice = Math.floor(Math.random()*3)
+const playGame=()=>{
+    let humanScore = 0;
+    let computerScore = 0;
 
-    switch (choice) {
-        case 0:
-            return "rock"
-        case 1:
-            return "paper"
-        case 2:
-            return "scissors"
-        default:
-            return "error"
-    }
-}
+     const getComputerChoice =()=>{
+        let choice = Math.floor(Math.random()*3)
 
-const getHumanChoice = () =>{
-    let choice = prompt("Your turn")
-    return choice.toLowerCase();
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-const playRound=(humanChoice,computerChoice)=>{
-    if(humanChoice === "rock"){
-        switch (computerChoice) {
-            case "paper":
-                return "You loose paper beats rock"
-            case "scissors":
-                return "You win rock beats scissors"
+        switch (choice) {
+            case 0:
+                return "rock"
+            case 1:
+                return "paper"
+            case 2:
+                return "scissors"
             default:
-                return "Oops Draw !"
+                return "error"
         }
     }
-    else if(humanChoice === "paper"){
-        switch (computerChoice) {
-            case "rock":
-                return "You win paper beats rock"
-            case "scissors":
-                return "You loose scissors beats paper"
-            default:
-                return "Oops Draw !"
+
+    const getHumanChoice = () =>{
+        let choice = prompt("Your turn")
+        return choice.toLowerCase();
+    }
+
+    const playRound=(humanChoice,computerChoice)=>{
+        if(humanChoice === "rock"){
+            switch (computerChoice) {
+                case "paper":
+                    computerScore ++;
+                    return "You loose paper beats rock"
+                case "scissors":
+                    humanScore++
+                    return "You win rock beats scissors"
+                default:
+                    return "Oops Draw !"
+            }
         }
+        else if(humanChoice === "paper"){
+            switch (computerChoice) {
+                case "rock":
+                    humanScore++
+                    return "You win paper beats rock"
+                case "scissors":
+                    computerScore++
+                    return "You loose scissors beats paper"
+                default:
+                    return "Oops Draw !"
+            }
+        }
+        else{
+            switch (computerChoice) {
+                case "rock":
+                    computerScore++
+                    return "You loose rock beats scissors"
+                case "paper":
+                    humanScore++
+                    return "You win scissors beats paper"
+                default:
+                    return "Oops Draw !"
+            }
+        }
+    }
+    const round = parseInt(prompt("Enter the number of rounds:- "))
+    for (let i = 0; i < round; i++)
+    {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(`Human : ${humanSelection}`)
+        console.log(`Computer : ${computerSelection}`)
+        console.log(playRound(humanSelection,computerSelection))
+        console.log(`Score
+            Human: ${humanScore} Computer: ${computerScore}`)
+    }
+        if (humanScore > computerScore)
+    {
+        console.log("Winner is Human")
+    }
+    else if(computerScore> humanScore)
+    {
+        console.log("Computer wins")
     }
     else{
-        switch (computerChoice) {
-            case "rock":
-                return "You loose rock beats scissors"
-            case "paper":
-                return "You win scissors beats paper"
-            default:
-                return "Oops Draw !"
-        }
+        console.log("Its a draw")
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(`Human : ${humanSelection}`)
-console.log(`Computer : ${computerSelection}`)
-console.log(playRound(humanSelection,computerSelection))
+playGame();
